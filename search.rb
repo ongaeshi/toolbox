4b9f030166b0a55916c1730e56362f6bb519c91e
ongaeshi <ongaeshi0621@gmail.com>
2023-07-03 00:33:04 +0900

    feat: edge で開けるようにする

--- search.rb
+++ search.rb
@@ -1,4 +1,10 @@
-require "toolbox"
+require "cgi"
 require "launchy"
 
-Launchy.open("https://www.google.co.jp/")
+query = CGI.escape(ARGV.join(" "))
+
+# Default Browser
+# Launchy.open("https://www.google.co.jp/search?q=#{query}")
+
+# Edge
+system("start msedge https://www.bing.com/search?q=#{query}")
